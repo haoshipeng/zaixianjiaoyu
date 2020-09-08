abb4a86da1c4e92c43556230889568e0cb9bbfc3
haoshipeng <787128455@qq.com>
2020-09-08 16:32:10 +0800

    在线教育后端接口

---
+++ guli-microservice-vod/src/main/java/com/guli/vod/VodApplication.java
@@ -0,0 +1,14 @@
+package com.guli.vod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源自动配置
+@ComponentScan(basePackages={"com.guli.vod","com.guli.common"})
+public class VodApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VodApplication.class);
+    }
+}
